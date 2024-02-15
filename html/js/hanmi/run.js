7e19d129c88060ee1742e32b1c38fd1009efce16
bonggi <chaebonggi@naver.com>
2024-02-15 13:58:11 +0900

    .

--- html/js/hanmi/run.js
+++ html/js/hanmi/run.js
@@ -197,22 +197,22 @@ $(document).ready(function() {
         }
     }); 
     
-    var swiper = new Swiper(".gpmu_list .con_list", {
-        spaceBetween: 20,
-        slidesPerView: 4,
-        autoplay: false,
-        loop: false,
-        breakpoints: {
-            1024: {
-                slidesPerView: 3
-            },
-            860: {
-                slidesPerView: 2,
-            },
-            640: {
-                slidesPerView: 1.3,
-                spaceBetween: 10,
-            },
-        }
-    });
+    // var swiper = new Swiper(".gpmu_list .con_list", {
+    //     spaceBetween: 20,
+    //     slidesPerView: 4,
+    //     autoplay: false,
+    //     loop: false,
+    //     breakpoints: {
+    //         1024: {
+    //             slidesPerView: 3
+    //         },
+    //         860: {
+    //             slidesPerView: 2,
+    //         },
+    //         640: {
+    //             slidesPerView: 1.3,
+    //             spaceBetween: 10,
+    //         },
+    //     }
+    // });
 });
