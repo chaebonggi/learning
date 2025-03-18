789b52828f4f69df19729faedad9bb9b66bacaba
bonggi <chaebonggi@naver.com>
2025-03-18 17:32:16 +0900

    .

--- html/js/hanmi/run.js
+++ html/js/hanmi/run.js
@@ -13,11 +13,6 @@ $(document).ready(function() {
         }, 100);
     });
 
-    $('#header .m_menu .search_btn').click(function () {
-        $(this).toggleClass('active');
-        $(this).next().toggle();
-    });
-
     let mainSwiper = new Swiper(".main_wrap", {
         slidesPerView: 1,
         spaceBetween: 30,
@@ -56,9 +51,61 @@ $(document).ready(function() {
         $('.main_wrap .control .swiper-button-pause').show();
     });
 
+    // // tab contents
+    /*function tabSlide() {
+        function initializeSwiper(tabId) {
+            swiper = new Swiper(`.gpmu_pick ${tabId} .con_list`, {
+                spaceBetween: 26,
+                slidesPerView: 4,
+                autoplay: false,
+                loop: false,
+                observer: true,
+                observeParents: true,
+                navigation: {
+                    nextEl: `.gpmu_pick ${tabId} .swiper-button-next`,
+                    prevEl: `.gpmu_pick ${tabId} .swiper-button-prev`,
+                },
+                pagination: {
+                    clickable : true,
+                    el: `.gpmu_pick ${tabId} .swiper-pagination`,
+                    type: 'bullets',
+                },
+                breakpoints: {
+                    1024: {
+                        slidesPerView: 3,
+                    },
+                    860: {
+                        slidesPerView: 2,
+                    },
+                    640: {
+                        slidesPerView: 1.3,
+                        spaceBetween: 10,
+                    },
+                },
+            });
+        }
+        initializeSwiper('#tab01');   
+        
+        $(".gpmu_pick .tab_container > .tab_content").hide();
+
+        $(".gpmu_pick .tab_container").each(function () {
+            $(this).children(".tab_content").first().show();
+        });
+        $(".gpmu_pick .cont_tabs li a").click(function (e) {
+            e.preventDefault();
+            $(this).parent().siblings("li").removeClass("active");
+            $(this).parent().addClass("active");
+            $(this).parents('.tab_container').find(".tab_content").hide();
+            let tabId = $(this).attr("href");
+            initializeSwiper(tabId, true);
+            $(tabId).fadeIn();        
+        });
+    }
+    tabSlide();      */ 
+
     var swiper = new Swiper(".gpmu_best .con_list", {
-        spaceBetween: 20,
-        slidesPerView: 4,
+        spaceBetween: 24,
+        slidesPerView: 'auto',
         autoplay: false,
         loop: false,
         navigation: {
@@ -70,15 +117,9 @@ $(document).ready(function() {
             el: '.gpmu_best .swiper-pagination',
             type: 'bullets',
         },
-        breakpoints: {
-            1024: {
-                slidesPerView: 3
-            },
-            860: {
-                slidesPerView: 2,
-            },
-            640: {
-                slidesPerView: 1.3,
+        breakpoints: { 
+        	1024: {
+            	slidesPerView: 'auto',
                 spaceBetween: 10,
             },
         }
@@ -146,8 +187,8 @@ $(document).ready(function() {
     }); 
     
     var swiper = new Swiper(".gpmu_etc .con_list", {
-        spaceBetween: 20,
-        slidesPerView: 4,
+    	spaceBetween: 24,
+        slidesPerView: 'auto',
         autoplay: false,
         loop: false,
         pagination: {
@@ -155,23 +196,19 @@ $(document).ready(function() {
             el: '.gpmu_etc .swiper-pagination',
             type: 'bullets',
         },
-        breakpoints: {
-            1024: {
-                slidesPerView: 3
-            },
-            860: {
-                slidesPerView: 2,
-            },
-            640: {
-                slidesPerView: 1.3,
+        breakpoints: { 
+        	1024: {
+            	slidesPerView: 'auto',
                 spaceBetween: 10,
             },
         }
     });
-
-    $('.privacyWrap .topBtn, .privacyWrap .privacyList a').click(function(e){
-        $("html, body").animate({ scrollTop: $(this.hash).offset().top }, 500);
-        e.preventDefault();
-    });
-
+    
+    $("#searchTextPc").keyup(function(event){
+    	console.log("F");
+	    if (event.keyCode === 13) {
+	      //event.preventDefault();
+	      goSearch('PC');
+	    }
+	  });
 });
